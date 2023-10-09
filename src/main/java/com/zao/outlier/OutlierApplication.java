e40eabc26402662d51008819eb51cf3051df4b97
ozoomosigho <ozo.omosigho@sainsburys.co.uk>
2023-10-09 08:10:12 +0100

    First commit - project initialisation

---
+++ src/main/java/com/zao/outlier/OutlierApplication.java
@@ -0,0 +1,13 @@
+package com.zao.outlier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutlierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutlierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zao/outlier/OutlierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zao.outlier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutlierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
